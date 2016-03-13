12ac369bbeedd2fa359b42f59aba49689d24bb21
eastmoon <chenjianneng1117@gmail.com>
2016-03-13 21:52:23 +0800

    add invitiation

---
+++ app/src/main/java/com/jared/emlazychat/domain/Invitation.java
@@ -0,0 +1,76 @@
+package com.jared.emlazychat.domain;
+
+/**
+ * Created by jared on 16/3/13.
+ */
+public class Invitation {
+    private long id;
+    private String owner;
+    private String account;
+    private String name;
+    private String icon;
+    private String content;
+    private boolean agree;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isAgree() {
+        return agree;
+    }
+
+    public void setAgree(boolean agree) {
+        this.agree = agree;
+    }
+
+    public String toString() {
+        return "Invitation [id=" + id + ", owner=" + owner + ", account="
+                + account + ", name=" + name + ", icon=" + icon + ", content="
+                + content + ", agree=" + agree + "]";
+    }
+}
